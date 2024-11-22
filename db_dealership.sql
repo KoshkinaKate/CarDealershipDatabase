85aadc8e93b63fe141ee6c5d6fed55d1d2866926
KoshkinaKate <koshkinaekaterina4987@gmail.com>
2024-11-22 13:20:37 -0500

    second table completed

--- db_dealership.sql
+++ db_dealership.sql
@@ -5,12 +5,24 @@ CREATE DATABASE car_dealership;
 USE car_dealership;
 
 DROP TABLE IF EXISTS dealership;
+DROP TABLE IF EXISTS vehicles;
 
 CREATE TABLE dealerships(
 DealershipID INT AUTO_INCREMENT PRIMARY KEY,
 `Name` VARCHAR(50),
 Address VARCHAR(50),
-Phone VARCHAR(12),
+Phone VARCHAR(12)
 );
 
+CREATE TABLE vehicles(
+Vin VARCHAR(30) PRIMARY KEY,
+`Year` DATE,
+Make VARCHAR(20),
+Model VARCHAR(20),
+VehicleType VARCHAR(20),
+Color VARCHAR(20),
+Odometer INT,
+Price FLOAT,
+Sold BOOLEAN
+);
 
