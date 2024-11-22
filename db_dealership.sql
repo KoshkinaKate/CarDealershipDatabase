850a7510b596f9e21bb16a1add4ede334b79e143
KoshkinaKate <koshkinaekaterina4987@gmail.com>
2024-11-22 14:05:02 -0500

    updated dates for vehicle insert

--- db_dealership.sql
+++ db_dealership.sql
@@ -61,26 +61,26 @@ VALUES ('BEST_CARS_HEEERE', '4568 Bold St, Harrison, NC, 15348', '456-098-0098')
 
 INSERT INTO vehicles (Vin, `Year`, Make, Model, VehicleType, Color, Odometer, Price, Sold)
 VALUES
-('1HGCM82633A123456', 2023, 'Toyota', 'Camry', 'Sedan', 'White', 15000, 24000.00, FALSE),
-('1HGCM82633A654321', 2022, 'Honda', 'Civic', 'Sedan', 'Black', 12000, 20000.00, TRUE),
-('1HGCM82633A112233', 2023, 'Ford', 'F-150', 'Truck', 'Blue', 25000, 45000.00, FALSE),
-('1HGCM82633A223344', 2021, 'Chevrolet', 'Malibu', 'Sedan', 'Red', 30000, 19000.00, FALSE),
-('1HGCM82633A334455', 2020, 'Jeep', 'Wrangler', 'SUV', 'Green', 35000, 32000.00, TRUE),
-('1HGCM82633A445566', 2023, 'Tesla', 'Model 3', 'Electric', 'Silver', 5000, 35000.00, FALSE),
-('1HGCM82633A556677', 2022, 'Nissan', 'Altima', 'Sedan', 'Gray', 18000, 22000.00, TRUE),
-('1HGCM82633A667788', 2021, 'Hyundai', 'Tucson', 'SUV', 'Blue', 22000, 27000.00, FALSE),
-('1HGCM82633A778899', 2020, 'BMW', 'X5', 'SUV', 'Black', 40000, 45000.00, TRUE),
-('1HGCM82633A889900', 2023, 'Audi', 'A4', 'Sedan', 'White', 10000, 39000.00, FALSE),
-('1HGCM82633A990011', 2022, 'Subaru', 'Outback', 'SUV', 'Silver', 15000, 31000.00, FALSE),
-('1HGCM82633A110022', 2021, 'Chevrolet', 'Silverado', 'Truck', 'Red', 35000, 41000.00, TRUE),
-('1HGCM82633A120033', 2020, 'Toyota', 'RAV4', 'SUV', 'Green', 30000, 29000.00, FALSE),
-('1HGCM82633A130044', 2023, 'Mazda', 'CX-5', 'SUV', 'Blue', 12000, 31000.00, FALSE),
-('1HGCM82633A140055', 2022, 'Kia', 'Soul', 'Hatchback', 'Gray', 14000, 20000.00, TRUE),
-('1HGCM82633A150066', 2021, 'GMC', 'Terrain', 'SUV', 'Black', 25000, 27000.00, FALSE),
-('1HGCM82633A160077', 2020, 'Mercedes-Benz', 'C-Class', 'Sedan', 'White', 45000, 38000.00, TRUE),
-('1HGCM82633A170088', 2023, 'Volkswagen', 'Jetta', 'Sedan', 'Silver', 8000, 25000.00, FALSE),
-('1HGCM82633A180099', 2022, 'Honda', 'Accord', 'Sedan', 'Blue', 16000, 27000.00, TRUE),
-('1HGCM82633A190110', 2021, 'Ford', 'Explorer', 'SUV', 'Black', 28000, 36000.00, FALSE);
+('1HGCM82633A123456', '2023-01-15', 'Toyota', 'Camry', 'Sedan', 'White', 15000, 24000.00, FALSE),
+('1HGCM82633A654321', '2022-06-10', 'Honda', 'Civic', 'Sedan', 'Black', 12000, 20000.00, TRUE),
+('1HGCM82633A112233', '2023-03-05', 'Ford', 'F-150', 'Truck', 'Blue', 25000, 45000.00, FALSE),
+('1HGCM82633A223344', '2021-11-20', 'Chevrolet', 'Malibu', 'Sedan', 'Red', 30000, 19000.00, FALSE),
+('1HGCM82633A334455', '2020-09-15', 'Jeep', 'Wrangler', 'SUV', 'Green', 35000, 32000.00, TRUE),
+('1HGCM82633A445566', '2023-05-25', 'Tesla', 'Model 3', 'Electric', 'Silver', 5000, 35000.00, FALSE),
+('1HGCM82633A556677', '2022-07-14', 'Nissan', 'Altima', 'Sedan', 'Gray', 18000, 22000.00, TRUE),
+('1HGCM82633A667788', '2021-02-28', 'Hyundai', 'Tucson', 'SUV', 'Blue', 22000, 27000.00, FALSE),
+('1HGCM82633A778899', '2020-03-10', 'BMW', 'X5', 'SUV', 'Black', 40000, 45000.00, TRUE),
+('1HGCM82633A889900', '2023-10-05', 'Audi', 'A4', 'Sedan', 'White', 10000, 39000.00, FALSE),
+('1HGCM82633A990011', '2022-12-01', 'Subaru', 'Outback', 'SUV', 'Silver', 15000, 31000.00, FALSE),
+('1HGCM82633A110022', '2021-08-18', 'Chevrolet', 'Silverado', 'Truck', 'Red', 35000, 41000.00, TRUE),
+('1HGCM82633A120033', '2020-04-07', 'Toyota', 'RAV4', 'SUV', 'Green', 30000, 29000.00, FALSE),
+('1HGCM82633A130044', '2023-06-30', 'Mazda', 'CX-5', 'SUV', 'Blue', 12000, 31000.00, FALSE),
+('1HGCM82633A140055', '2022-01-22', 'Kia', 'Soul', 'Hatchback', 'Gray', 14000, 20000.00, TRUE),
+('1HGCM82633A150066', '2021-05-15', 'GMC', 'Terrain', 'SUV', 'Black', 25000, 27000.00, FALSE),
+('1HGCM82633A160077', '2020-08-24', 'Mercedes-Benz', 'C-Class', 'Sedan', 'White', 45000, 38000.00, TRUE),
+('1HGCM82633A170088', '2023-04-11', 'Volkswagen', 'Jetta', 'Sedan', 'Silver', 8000, 25000.00, FALSE),
+('1HGCM82633A180099', '2022-02-15', 'Honda', 'Accord', 'Sedan', 'Blue', 16000, 27000.00, TRUE),
+('1HGCM82633A190110', '2021-09-03', 'Ford', 'Explorer', 'SUV', 'Black', 28000, 36000.00, FALSE);
 
 INSERT INTO inventory (DealershipID, Vin)
 VALUES
